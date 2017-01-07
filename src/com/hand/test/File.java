d37552e853a88922313d703bd40fe7d158d47197
wantao.cheng <2467192197@qq.com>
2017-01-07 16:01:11 +0800

    修复完bug，恢复工作 --开发者1

--- src/com/hand/test/File.java
+++ src/com/hand/test/File.java
@@ -8,6 +8,8 @@ public class File {
 		System.out.println("第二次修改");
 		System.out.println("第三次修改 --开发者2");
 		System.out.println("紧急修复bug， 修复成功！");
+		System.out.println("开始工作了 --开发者1");
+		System.out.println("有重新开始工作了");
 	}
 
 }
